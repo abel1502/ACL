c54b0d2e8f40788799bb4e718230ea9559df031f
abel1502 <terragame.abel@gmail.com>
2022-03-09 11:38:46 +0300

    Add a ut proj

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+#include <ACL/general.h>
+#include <ACL/log.h>
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests {
+	TEST_CLASS(UnitTests) {
+	public:
+		TEST_METHOD(TestMethod1) {
+			//
+		}
+	};
+}
